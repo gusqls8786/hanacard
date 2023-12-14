906eb7a26a99ded75f90683a5c3469529e573be1
gusqls8786 <gusqls8786@gmail.com>
2023-12-14 17:26:47 +0900

    12.14

--- js/main.js
+++ js/main.js
@@ -1,70 +1,77 @@
-$(document).ready(function(){
-    $('.eb_slider_list').slick({
+    // slick
+    let eb_slider = $('.eb_slider_list')
+    eb_slider.slick({
         arrows:false,
         dots:true,
         autoplay:true,
         pauseOnHover:false,
-        fade:true
+        fade:true,
     });
-  });
 
-// 정지 재생 버튼
-// 자바스크립트
-// btn_stop.addEventListener('click'function(){})
-// btn_stop.onclick(function(){})
+    //정지/재생 버튼
+    // 선택자.addEventListerner('click',function(){})
+    // 선택자.onclick(function(){})
+    $('.event_box2 .page_act .btn_stop').click(function(e){
+        e.preventDefault();
+        if($('.event_box2 .page_act').hasClass('on')){//on이라는 클래스명을 가지고 있다면 tru, 없다면 false
+            $('.event_box2 .page_act').removeClass('on');
+            eb_slider.slick('slickPause');
 
-// jquary
-$('.event_box_2 .page_act .btn_stop').click(function(e){
-    e.preventDefault();
-    if($('.event_box_2 .page_act').hasClass('on')){
-        // on이라는 클래스명을 가지고있다면 true
-        // 없으면 false
-        $('.event_box_2 .page_act').removeClass('on');
-        $('.eb_slider_list').slick('slickPause');
-    }
-})
+        }
+    })
 
-$('.event_box_2 .page_act .btn_play').click(function(e){
-    e.preventDefault();
-    if(!$('.event_box_2 .page_act').hasClass('on')){
-      $('.event_box_2 .page_act').addClass('on')
-      $('.eb_slider_list').slick('slickPlay');
-    } 
-})
+    $('.event_box2 .page_act .btn_play').click(function(e){
+        e.preventDefault();
+        if(!$('.event_box2 .page_act').hasClass('on')){
 
-$('.card').slick({
-    arrows:false,
-    autoplay:true,
-    dots:true,
-    pauseOnHover:false,
-  });
+            //! --> not   !! --> 긍정   
+        $('.event_box2 .page_act').addClass('on');
+        eb_slider.slick('slickPlay');
+        }
+    })
 
-  $('.event_box_3 .page_act .btn_stop').click(function(e){
-    e.preventDefault();
-    if($('.event_box_3 .page_act').hasClass('on')){
-        // on이라는 클래스명을 가지고있다면 true
-        // 없으면 false
-        $('.event_box_3 .page_act').removeClass('on');
-        $('.card').slick('slickPause');
-    }
-})
 
-$('.event_box_3 .page_act .btn_play').click(function(e){
-    e.preventDefault();
-    if(!$('.event_box_3 .page_act').hasClass('on')){
-      $('.event_box_3 .page_act').addClass('on')
-      $('.card').slick('slickPlay');
-    } 
-})
+    $('.card').slick({
+        arrows:false,
+        dots:true,
+        autoplay:true,
+        pauseOnHover:false,
+        fade:true,
+      });
 
-// life button
+      
+    //정지/재생 버튼
+    // 선택자.addEventListerner('click',function(){})
+    // 선택자.onclick(function(){})
+    $('.event_box3 .page_act .btn_stop').click(function(e){
+        e.preventDefault();
+        if($('.event_box3 .page_act').hasClass('on')){//on이라는 클래스명을 가지고 있다면 true, 없다면 false
+            $('.event_box3 .page_act').removeClass('on');
+            $('.card').slick('slickPause');
 
-let tab = $('.life_menu ul li')
-let menu = $('.life_style ul li')
+        }
+    })
 
-tab.mouseover(function(){
-  let target = $(this)
-  let index = target.index()
-  menu.css({opacity:0})
-  menu.eq(index).css({opacity:1})
-})
+    $('.event_box3 .page_act .btn_play').click(function(e){
+        e.preventDefault();
+        if(!$('.event_box3 .page_act').hasClass('on')){
+
+            //! --> not   !! --> 긍정   
+        $('.event_box3 .page_act').addClass('on');
+        $('.card').slick('slickPlay');
+        }
+    })
+
+    // life 버튼
+    let tab=$('.life_menu ul li');
+    let menu=$('.life_style ul li');
+
+    tab.mouseover(function(){
+        let target = $(this);
+        let index = target.index();
+        console.log(index)
+        menu.css({opacity:0})
+        menu.eq(index).css({opacity:1})
+    });
+
+    
